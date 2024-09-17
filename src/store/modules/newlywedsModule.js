86987ebc33886553e8489332afbd3f1822ba3168
pavelbekarev <bekarev.ps@gmail.com>
2024-09-17 22:14:30 +0500

    adding dist subtree

---
+++ src/store/modules/newlywedsModule.js
@@ -0,0 +1,32 @@
+import marryInfo from "../../api/marryInfo";
+
+const state = {
+    all: []
+}
+
+
+const getters = {}
+
+const actions = {
+    getNewlywedsInfo({ commit }) {
+        marryInfo.getNewlyweds(newlyweds => {
+            commit('setNewlyweds', newlyweds);
+        });
+    }
+}
+
+
+const mutations = {
+    setNewlyweds(state, newlyweds) {
+        state.all = newlyweds;
+    }
+}
+
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
